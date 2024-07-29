6e11a60fa253890fabdf4f4a4743b3e9773d1ecc
asing44 <adamwarsing@gmail.com>
2024-07-29 01:13:29 -0400

    Project 3 Setup v1.0.0

---
+++ Front-End-Project-3/src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import Buttons from "./components/Buttons.vue";
+</script>
+
+<template>
+  <h1>FreeCodeCamp Drum Player!</h1>
+  <main>
+    <Buttons></Buttons>
+  </main>
+</template>
+
+<style scoped>
+
+h1 {
+  margin-top: 3.2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ Front-End-Project-3/src/components/Buttons.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Button from './ButtonItem.vue';
+</script>
+
+<template>
+  <Button>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Documentation</template>
+
+    Vue’s
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
+    provides you with all information you need to get started.
+  </Button>
+
+</template>
