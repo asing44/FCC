848b17f75fa2aa4ac81e0409456a46b5e12f0d13
Adam Warsing <109570966+asing44@users.noreply.github.com>
2025-01-06 17:47:58 -0500

    fix: Enhance author attribution styling in MainApp.vue

---
+++ .history/Front-End-Projects/Calculator/src/MainApp_20250106174705.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Calculator from './CalculatorMain.vue';
+</script>
+
+<template>
+  <main>
+    <Calculator class="calculator" />
+  </main>
+  <small>Created by <strong><a class="link_attribution" href="https://github.com/asing44">Adam Warsing</a></strong></small>
+</template>
+
+<style scoped>
+
+</style>
